535e176bff84c5369019ebb2780fa9b6f5cfe3f0
mare1997 <radojkovicmarko55@gmail.com>
2019-01-31 19:45:00 +0100

    First commit

---
+++ src/main/java/com/pi/PoslovnaInformatika/PoslovnaInformatikaApplication.java
@@ -0,0 +1,14 @@
+package com.pi.PoslovnaInformatika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoslovnaInformatikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoslovnaInformatikaApplication.class, args);
+	}
+
+}
+
