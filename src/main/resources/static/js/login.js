e8333ee2469e7fd997e23933b3c9ebc2d58e73ec
Maki <makibijelic3377@gmail.com>
2019-02-16 21:07:40 +0100

    login

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,45 @@
+function login(){
+	var username=$('#usernameLog').val().trim();
+	var pass =$('#passLog').val().trim();
+	if (username=="" || pass==""){
+		alert("Nije uneto korisnicko ime ili lozinka!!!")
+		return;
+	}
+	var data = {
+			'username':username,
+			'password':pass
+	}
+	console.log(data);
+	console.log("login");
+
+
+	$.ajax({
+		type: 'POST',
+		contentType: 'application/json',
+		url: 'https://localhost:8081/api/auth/login',
+		data: JSON.stringify(data),
+		dataType: 'json',
+		crossDomain:true,
+		cache:false,
+		processData:false,
+		success: function(response){
+			var token = response.access_token;
+			console.log(token);
+			console.log(response);
+
+			localStorage.setItem("token",token);
+			console.log(username);
+			localStorage.setItem("username",username);
+			console.log("login i ajax success");
+
+			window.location.replace("izborPPG.html");
+		},
+		error: function(jqXHR, textstatus, errorThrown){
+
+			if(jqXHR.status == "401"){
+				alert("Pogresno uneto korisnicko ime ili lozinka!!!")
+			}
+		}
+
+	});
+}
