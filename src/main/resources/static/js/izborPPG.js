2e4f972cec62e874296c985a742f71a97efc2dc5
Maki <makibijelic3377@gmail.com>
2019-02-20 17:49:45 +0100

    sredjen odabir poslovne godine i preduzeca

--- src/main/resources/static/js/izborPPG.js
+++ src/main/resources/static/js/izborPPG.js
@@ -1,10 +1,11 @@
 var token= localStorage.getItem("token");
 
 $(document).ready(function() {
-	loadPreduzece();
-  loadPG();
-	var auth = localStorage.getItem("authority")
+	var auth = localStorage.getItem("authority");
 	console.log(auth);
+	loadPreduzece();
+	loadPG1();
+	
 });
 
 function loadPreduzece(){
@@ -17,13 +18,12 @@ function loadPreduzece(){
     success: function (response) {
       var select = $('#preduzeca');
       for (var i=0; i<response.length; i++){
-        preduzece = response[i];
-
-          select.append(
-            '<option value="'+preduzece.id+'">'+preduzece.name+'</option>'
-          )
-
+    	  pr = response[i];
+    	  select.append(
+    	            '<option value="'+pr.id+'" >'+pr.name+'</option>');
+    	  
       }
+      
     },error: function (jqXHR, textStatus, errorThrown) {
 			alert("read error!!!");
   }
@@ -31,8 +31,19 @@ function loadPreduzece(){
 }
 
 function loadPG(){
+	
+	var pred = document.getElementById("preduzeca");
+	
+	var status= pred.options[pred.selectedIndex].value;
+	
+	var pg = document.getElementById("pg");
+	
+	pg.options.length = 0;
+	
+	
+	console.log(status);
   $.ajax({
-    url:'https://localhost:8081/api/poslovnagodina/all',
+    url:'https://localhost:8081/api/poslovnagodina/all/'+ status,
     headers:{Authorization:"Bearer " + token},
     type:"GET",
     dataType: 'json',
@@ -53,13 +64,42 @@ function loadPG(){
 });
 }
 
+
+function loadPG1(){
+
+	console.log(status);
+  $.ajax({
+    url:'https://localhost:8081/api/poslovnagodina/all/'+ 1,
+    headers:{Authorization:"Bearer " + token},
+    type:"GET",
+    dataType: 'json',
+    crossDomain:true,
+    success: function (response) {
+      var select = $('#pg');
+      for (var i=0; i<response.length; i++){
+        pg = response[i];
+
+          select.append(
+            '<option value="'+pg.id+'">'+pg.godina+'</option>'
+          )
+
+      }
+    },error: function (jqXHR, textStatus, errorThrown) {
+			alert("read error!!!");
+  }
+});
+}
+
+
+
 function proceed(){
-	var selectP = document.getElementById('preduzeca');
+	
 	var selectPG = document.getElementById('pg');
 	var pgId= selectPG.options[selectPG.selectedIndex].value;
+	var selectP = document.getElementById("preduzeca");
 	var pId= selectP.options[selectP.selectedIndex].value;
 	console.log("id pg: " + pgId + "id pred: " + pId);
 	localStorage.setItem("pgId",pgId);
-	localStorage.setItem("pId",pId);
 	window.location.replace("mainPage.html");
 }
+
