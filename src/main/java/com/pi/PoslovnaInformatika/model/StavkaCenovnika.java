acaddb3356dc50111352c1ab9484220901e4533e
Stefan Ilić <ilagila1997@gmail.com>
2019-03-25 14:36:57 +0100

    Narudzbenica ispravljena greska kod citanja stavki cenovnika
    U modelu Stavke cenovnika promenjena kolona na koju se cilja za vezu sa
    robom,u js ispravljeno ciljanje na odredjene stavke cenovnika

--- src/main/java/com/pi/PoslovnaInformatika/model/StavkaCenovnika.java
+++ src/main/java/com/pi/PoslovnaInformatika/model/StavkaCenovnika.java
@@ -34,7 +34,7 @@ public class StavkaCenovnika implements Serializable {
 	
 	
 	@ManyToOne
-	@JoinColumn(name = "cena_id", referencedColumnName = "roba_id", nullable = true)
+	@JoinColumn(name = "roba_id", referencedColumnName = "roba_id", nullable = true)
 	private Roba roba;
 	
 	@ManyToOne
